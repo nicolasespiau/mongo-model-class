aba309b6130006ca66aa55df636312a9d92cf7c9
Nicolas Espiau <contact@stilll.fr>
2020-03-04 01:36:59 +0100

    Added libs and index

---
+++ index.js
@@ -0,0 +1,9 @@
+const Cache = require('./lib/cacheClient');
+const SMModel = require('./lib/Model.class');
+const SMCollection = require('./lib/Collection.class');
+
+module.exports = {
+  Cache,
+  SMModel,
+  SMCollection
+};
